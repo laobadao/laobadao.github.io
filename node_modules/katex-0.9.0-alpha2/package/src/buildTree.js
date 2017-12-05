8a4af1e02eb09fe51e0b1304bdf894f7346e1251
2296616900@qq.com <2296616900@qq.com>
2017-12-05 09:29:24 +0800

    latex go on

---
+++ node_modules/katex-0.9.0-alpha2/package/src/buildTree.js
@@ -0,0 +1,46 @@
+// @flow
+import buildHTML from "./buildHTML";
+import buildMathML from "./buildMathML";
+import buildCommon from "./buildCommon";
+import Options from "./Options";
+import Settings from "./Settings";
+import Style from "./Style";
+
+import type ParseNode from "./ParseNode";
+import type domTree from "./domTree";
+
+const buildTree = function(
+    tree: ParseNode[],
+    expression: string,
+    settings: Settings,
+): domTree.span {
+    settings = settings || new Settings({});
+
+    let startStyle = Style.TEXT;
+    if (settings.displayMode) {
+        startStyle = Style.DISPLAY;
+    }
+
+    // Setup the default options
+    const options = new Options({
+        style: startStyle,
+        maxSize: settings.maxSize,
+    });
+
+    // `buildHTML` sometimes messes with the parse tree (like turning bins ->
+    // ords), so we build the MathML version first.
+    const mathMLNode = buildMathML(tree, expression, options);
+    const htmlNode = buildHTML(tree, options);
+
+    const katexNode = buildCommon.makeSpan(["katex"], [
+        mathMLNode, htmlNode,
+    ]);
+
+    if (settings.displayMode) {
+        return buildCommon.makeSpan(["katex-display"], [katexNode]);
+    } else {
+        return katexNode;
+    }
+};
+
+export default buildTree;
